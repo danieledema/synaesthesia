f73bf184a6e11cd033a84ab4b0f4e8476c53d263
Daniele De Martini <daniele@robots.ox.ac.uk>
2024-07-10 11:04:00 +0100

    moved csv dataset in rel library

---
+++ src/synaesthesia/csv_dataset.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import pandas
+
+from src.vigil2.data.abstract_dataset import DatasetBase
+
+
+class CsvDataset(DatasetBase):
+    def __init__(self, path):
+        super().__init__()
+
+        self.path = Path(path)
+        self.data = pandas.read_csv(self.path)
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_data(self, idx):
+        data = {
+            f"{col}": self.data[col].values[idx]
+            for col in self.data.columns
+            if col != "timestamp"
+        }
+        return data
+
+    @property
+    def sensor_ids(self):
+        return [f"{self.id}-{col}" for col in self.data.columns if col != "timestamp"]
+
+    def get_timestamp(self, idx):
+        return self.data["timestamp"].values[idx]
+
+    def get_timestamp_idx(self, timestamp):
+        return self.data[self.data["timestamp"] == timestamp].index[0]
+
+    @property
+    def timestamps(self):
+        return self.data["timestamp"].values.tolist()
