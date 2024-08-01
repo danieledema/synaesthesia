cf4423fdde001c00d2093cbc38540c1f249ef0b7
Daniele De Martini <daniele@robots.ox.ac.uk>
2024-08-01 10:49:00 +0100

    almost finished

--- src/synaesthesia/abstract/multi_signal_dataset.py
+++ src/synaesthesia/abstract/multi_signal_dataset.py
@@ -5,16 +5,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from .conversion import convert_to_timestamp
 from .dataset_base import DatasetBase
-
-
-def convert_to_datetime(timestamp):
-    # Adjust this function based on the format of your timestamps
-    try:
-        return datetime.strptime(timestamp, "%Y%m%dT%H%M")
-    except ValueError:
-        return datetime.strptime(timestamp, "%Y%m%dT%H%M%S%f")
 
 
 class MultiSignalDataset(DatasetBase):
@@ -48,120 +39,130 @@ class MultiSignalDataset(DatasetBase):
         self.patch = patch
 
         # Create a DataFrame to store timestamps and corresponding indices
-        self.timestamp_df = self._initialize_timestamp_df()
+        self._timestamps = self._initialize_timestamps()
+        self.data_dict = self._initialize_data_dict()
 
-        # Fill missing timestamps based on the fill method
-        self._fill_missing_timestamps()
-
-        self._timestamps = [np.datetime64(ts, "ns") for ts in self.timestamp_df.index]
-
-    def _initialize_timestamp_df(self) -> pd.DataFrame:
+    def _initialize_timestamps(self) -> list[int]:
         """
         Initializes the DataFrame to store timestamps and corresponding indices.
         """
-        print("Initializing timestamp DataFrame...")
 
         if self.aggregation == "all":
-            # For 'all' aggregation, mark each timestamp present in each dataset
-            print("Aggregating timestamps (method: all)...")
-            # Initialize an empty DataFrame with timestamps from all datasets
-            all_timestamps = pd.concat(
-                [
-                    pd.Series(ds.timestamps, name="timestamp")
-                    for ds in self.single_signal_datasets
+            merged_timestamps = self.single_signal_datasets[0].timestamps
+
+            for ds in self.single_signal_datasets[1:]:
+                i, j = 0, 0
+
+                timestmaps_to_merge = ds.timestamps
+                tmp_merged_timestamps = [
+                    min(merged_timestamps[0], timestmaps_to_merge[0])
                 ]
-            ).reset_index(drop=True)
 
-            # Remove duplicates if any
-            all_timestamps = all_timestamps.drop_duplicates()
+                while i < len(merged_timestamps) and j < len(timestmaps_to_merge):
 
-            # Initialize the DataFrame with NaNs
-            timestamp_df = pd.DataFrame(
-                index=all_timestamps,
-                columns=[
-                    f"dataset_{i}" for i in range(len(self.single_signal_datasets))
-                ],
-            )
+                    if tmp_merged_timestamps[-1] == merged_timestamps[i]:
+                        i += 1
+                        continue
+
+                    if tmp_merged_timestamps[-1] == timestmaps_to_merge[j]:
+                        j += 1
+                        continue
+
+                    if merged_timestamps[i] < timestmaps_to_merge[j]:
+                        tmp_merged_timestamps.append(merged_timestamps[i])
+                        i += 1
+                    else:
+                        tmp_merged_timestamps.append(timestmaps_to_merge[j])
+                        j += 1
+
+                # Append any remaining elements from list1 or list2
+                tmp_merged_timestamps.extend(merged_timestamps[i:])
+                tmp_merged_timestamps.extend(timestmaps_to_merge[j:])
+                merged_timestamps = tmp_merged_timestamps
+
+            return merged_timestamps
 
         elif self.aggregation == "common":
+            merged_timestamps = self.single_signal_datasets[0].timestamps
 
-            # For 'common' aggregation, mark timestamps common to all datasets
-            print("Aggregating timestamps (method: common)...")
+            for ds in self.single_signal_datasets[1:]:
+                to_delete = []
+                for i, t in enumerate(merged_timestamps):
+                    if t not in ds:
+                        to_delete.append(i)
 
-            common_timestamps = set.intersection(
-                *[set(ds.timestamps) for ds in self.single_signal_datasets]
-            )
-            common_timestamps = sorted(
-                common_timestamps
-            )  # Convert set to a sorted list
+                for i in reversed(to_delete):
+                    del merged_timestamps[i]
 
-            timestamp_df = pd.DataFrame(
-                index=common_timestamps,
-                columns=[
-                    f"dataset_{i}" for i in range(len(self.single_signal_datasets))
-                ],
-            )
+            return merged_timestamps
 
         elif self.aggregation.startswith("I:"):
-
             # For 'I:<idx>' aggregation, mark timestamps from a specific dataset
             idx = int(self.aggregation[2:])
-            print(f"Aggregating timestamps (method: I:{idx})...")
-            timestamp_df = pd.DataFrame(
-                index=self.single_signal_datasets[idx].timestamps,
-                columns=[
-                    f"dataset_{i}" for i in range(len(self.single_signal_datasets))
-                ],
-            )
+            return self.single_signal_datasets[idx].timestamps
 
+        else:
+            raise ValueError(f"Invalid aggregation method: {self.aggregation}")
+
+    def _initialize_data_dict(self) -> dict[int, list[int | None]]:
+        """
+        Fills data vs timestamps based on the specified fill method.
+        """
+
+        data_dict = {
+            t: [None] * len(self.single_signal_datasets) for t in self.timestamps
+        }
         for i, ds in enumerate(self.single_signal_datasets):
-            print(f"Processing dataset {i}...")
-            for j, timestamp in enumerate(tqdm(ds.timestamps)):
-                if timestamp in timestamp_df.index:
-                    timestamp_df.loc[timestamp, f"dataset_{i}"] = j
-
-        return timestamp_df
-
-    def _fill_missing_timestamps(self):
-        """
-        Fills missing timestamps based on the specified fill method.
-        """
-        print("Filling missing timestamps...")
+            for j, timestamp in enumerate(ds.timestamps):
+                if timestamp in data_dict:
+                    data_dict[timestamp][i] = j
 
         if self.fill == "none":
-            # No filling required
-            print("Fill method: none")
-            # self.timestamp_df = self.timestamp_df.sort_values()
+            return data_dict
 
         elif self.fill == "last":
-            # Fill missing timestamps with data from the last available timestamp
-            print("Fill method: last")
-            # self.timestamp_df = self.timestamp_df.sort_values()
-            self.timestamp_df = self.timestamp_df.fillna(method="ffill")
+            min_common_timestamp = max(
+                [ds.get_timestamp(0) for ds in self.single_signal_datasets]
+            )
+            for i in range(len(self.timestamps)):
+                if self.timestamps[i] < min_common_timestamp:
+                    data_dict[self.timestamps[i + 1]] = data_dict[self.timestamps[i]]
+                    del data_dict[self.timestamps[i]]
+                    del self.timestamps[i]
+
+            for i, ds in enumerate(self.single_signal_datasets):
+                for j, timestamp in enumerate(self.timestamps):
+                    if data_dict[timestamp][i] is None:
+                        data_dict[timestamp][i] = data_dict[self.timestamps[j - 1]][i]
+
+            return data_dict
 
         elif self.fill == "closest":
-            # Fill missing timestamps with data from the closest available timestamp within time_cut minutes
-            print("Fill method: closest")
-            for i in range(len(self.timestamp_df.columns)):
-                # Sort timestamps to ensure the interpolate('nearest') works correctly
-                timestamp_series = self.timestamp_df.index
-                limit = int(
-                    self.time_cut / (timestamp_series[1] - timestamp_series[0]).seconds
-                )  # Convert time_cut from minutes to seconds.
-                interpolated_column = (
-                    self.timestamp_df.iloc[:, i]
-                    .reindex(timestamp_series)
-                    .interpolate(method="nearest", limit=limit, limit_direction="both")
-                )
-                self.timestamp_df.iloc[:, i] = interpolated_column
+            for i, ds in enumerate(self.single_signal_datasets):
+                global_timestamp_idx, ds_timestamp_idx = 0, 1
 
-                if self.patch == "drop":
-                    print("Dropping timestamps that couldn't be filled...")
-                    # Remove timestamps that couldn't be filled (still None after interpolation)
-                    self.timestamp_df = self.timestamp_df.dropna()
+                while global_timestamp_idx < len(self.timestamps):
+                    timestamps_to_fill = self.timestamps[global_timestamp_idx]
+
+                    while (
+                        ds.get_timestamp(ds_timestamp_idx - 1) < timestamps_to_fill
+                        and ds.get_timestamp(ds_timestamp_idx) < timestamps_to_fill
+                    ):
+                        ds_timestamp_idx += 1
+
+                    if abs(
+                        ds.get_timestamp(ds_timestamp_idx - 1) - timestamps_to_fill
+                    ) < abs(ds.get_timestamp(ds_timestamp_idx) - timestamps_to_fill):
+                        data_dict[timestamps_to_fill][i] = ds_timestamp_idx - 1
+                    else:
+                        data_dict[timestamps_to_fill][i] = ds_timestamp_idx
+
+                    global_timestamp_idx += 1
+
+            return data_dict
 
     @property
-    def timestamps(self) -> List[np.datetime64]:
+    def timestamps(self) -> List[int]:
         """
         Returns the list of timestamps in numpy.datetime64 format.
         """
@@ -183,11 +184,18 @@ class MultiSignalDataset(DatasetBase):
         Returns:
             dict: Dictionary containing data from all datasets at the specified timestamp.
         """
-        data_slice = self.timestamp_df.iloc[idx]
+        data_slice = self.data_dict[self.timestamps[idx]]
 
         data_dict = {}
         for i, ds in enumerate(self.single_signal_datasets):
-            data_dict[f"{ds.id}"] = ds.get_data(data_slice[f"dataset_{i}"])
+            if data_slice[i] is None:
+                for k in ds.sensor_ids:
+                    data_dict[f"{ds.satellite_name}_{ds.id}-{k}"] = None
+            else:
+                data = ds.get_data(data_slice[i])
+
+                for k in data:
+                    data_dict[f"{ds.satellite_name}_{ds.id}-{k}"] = data[k]
 
         return data_dict
 
@@ -235,4 +243,4 @@ class MultiSignalDataset(DatasetBase):
         """
         Returns the ID of the dataset.
         """
-        return "MultiSignalDataset"
+        return ""
