891f88fda533885f4d4be5852ca2545d0f5524c5
luigicampanaro <luigi@oxfordrobotics.institute>
2024-09-25 19:16:15 +0100

    fix: fixing idxs

--- src/synaesthesia/abstract/sequential_dataset.py
+++ src/synaesthesia/abstract/sequential_dataset.py
@@ -34,8 +34,10 @@ class SequentialDataset(DatasetBase):
 
     @property
     def idxs(self) -> list[int]:
-        return [i * self.stride for i in range(len(self.dataset) // self.stride)]
-
+        len_samples = self.idx_format[-1] + 1
+        max_start_index = len(self.dataset) - len_samples  # Calculate the maximum starting index for valid sequences
+        return [i * self.stride for i in range(max_start_index // self.stride + 1)]  # Generate valid starting points
+    
     def __len__(self) -> int:
         len_samples = self.idx_format[-1] + 1
         return (len(self.dataset) - len_samples) // self.stride + 1
