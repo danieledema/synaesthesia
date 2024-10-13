69e62d105069313d0539e2b18d3dc7cd4ecf1fd0
Daniele De Martini <daniele@robots.ox.ac.uk>
2024-10-13 01:14:43 +0100

    added documentation

--- src/synaesthesia/abstract/dataset_base.py
+++ src/synaesthesia/abstract/dataset_base.py
@@ -4,6 +4,39 @@ from torch.utils.data import Dataset
 
 
 class DatasetBase(Dataset):
+    """
+    Base class for all datasets. It is a subclass of torch.utils.data.Dataset.
+    It is an abstract class and should be subclassed by all datasets.
+
+    Methods:
+    --------
+    get_data(idx: int) -> dict[str, Any]:
+        Returns the data at the specified index as a dictionary, with the special key 'idx' containing
+        the index and 'timestamp' containing the timestamp of the data.
+        If the dataset has an ID, the keys in the dictionary should be prefixed with the ID.
+
+    get_timestamp(idx: int) -> int:
+        Returns the timestamp at the specified index. Raises an IndexError if the index is out of range.
+
+    get_timestamp_idx(timestamp: int) -> int:
+        Given a timestamp, returns the index corresponding to that timestamp in the dataset.
+        Raises a ValueError if the timestamp is not found.
+
+    id -> str (property):
+        Returns a string combining the name of the sensor the dataset is associated with.
+
+    machine_name -> str (property):
+        Returns the machine name associated with the dataset.
+
+    timestamps -> list[int] (property):
+        Returns a list of timestamps in the dataset.
+
+    sensor_ids -> list[str] (property):
+        Returns a list containing the sensor reading in the dataset.
+        For example, for a dataset containing temperature and humidity readings, this would return
+        ['temperature', 'humidity'].
+    """
+
     def __getitem__(self, idx):
         data_sample = {"idx": idx, "timestamp": self.get_timestamp(idx)}
 
