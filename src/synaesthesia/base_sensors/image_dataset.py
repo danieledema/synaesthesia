fe5632d22999167b2752bb40cb68f1d59ea892b2
Daniele De Martini <daniele@robots.ox.ac.uk>
2024-11-07 00:03:00 +0000

    extracted multi file dataset form image dataset as superclass

--- src/synaesthesia/base_sensors/image_dataset.py
+++ src/synaesthesia/base_sensors/image_dataset.py
@@ -4,10 +4,10 @@ from typing import Any
 import numpy as np
 from PIL import Image
 
-from ..abstract.dataset_base import DatasetBase
+from .multi_file_dataset import MultiFileDataset
 
 
-class ImageDataset(DatasetBase):
+class ImageDataset(MultiFileDataset):
     """
     Dataset class for Image data.
     """
@@ -18,33 +18,9 @@ class ImageDataset(DatasetBase):
         extension: str,
         format: str = "RGB",
     ):
-        super().__init__()
+        super().__init__(folder_path, extension)
 
-        self.folder_path = Path(folder_path)
-        self.extension = extension
         self.format = format
-
-        files = self.folder_path.glob(f"*.{self.extension}")
-        self.files = list(files)
-        self.files.sort()
-
-        self._timestamps = [self.parse_filename(f) for f in self.files]
-        self.data_dict = {t: f for t, f in zip(self._timestamps, self.files)}
-
-    def parse_filename(self, filename) -> int:
-        raise NotImplementedError
-
-    @property
-    def timestamps(self):
-        return self._timestamps
-
-    def __len__(self) -> int:
-        return len(self.timestamps)
-
-    def get_data(self, idx) -> dict[str, Any]:
-        timestamp = self.get_timestamp(idx)
-        data = {self.sensor_ids[0]: self.read_data(self.data_dict[timestamp])}
-        return data
 
     def read_data(self, file_path: Path) -> Any:
         image = Image.open(file_path)
@@ -57,16 +33,7 @@ class ImageDataset(DatasetBase):
         if len(image_np.shape) == 3:
             image_np = image_np.transpose(2, 0, 1)
 
-        return image_np
-
-    def get_timestamp(self, idx):
-        return self.timestamps[idx]
-
-    def get_timestamp_idx(self, timestamp):
-        try:
-            return self._timestamps.index(timestamp)
-        except ValueError:
-            raise ValueError("Timestamp not found in dataset")
+        return {"RGB": image_np}
 
     @property
     def sensor_ids(self) -> list[str]:
